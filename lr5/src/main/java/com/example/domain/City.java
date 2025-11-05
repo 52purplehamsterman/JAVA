648c66b8cb6384da70ad0c6ab9d050ea027b8073
52purplehamsterman <2411ars2@gmail.com>
2025-11-05 17:07:58 +0300

    закинул 5 лабу

---
+++ lr5/src/main/java/com/example/domain/City.java
@@ -0,0 +1,35 @@
+package com.example.domain;
+
+public class City {
+    private String name;
+    private int population;
+
+    public City() {}
+
+    public City(String name, int population) {
+        this.name = name;
+        this.population = population;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public String getInfo() {
+        return "Город: " + name + ", население: " + population;
+    }
+}
+
+
